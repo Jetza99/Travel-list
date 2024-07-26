d5fad27124551b5cb5e9019b0c9bbc632be2182e
Omar Marnissi <omarmarnissi777@gmail.com>
2024-07-26 16:26:12 +0100

    finished project

---
+++ src/components/Item.js
@@ -0,0 +1,15 @@
+export default function Item({ item, onHandleDeleteItem, onHandleToggleItem }) {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => onHandleToggleItem(item.id)}
+      />
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button onClick={() => onHandleDeleteItem(item.id)}>❌</button>
+    </li>
+  );
+}
